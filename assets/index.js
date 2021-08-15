6c83a9b24b2a0d7ba58c6e97665b6f75938c57c7
ada <axu1122@gmail.com>
2021-08-14 21:24:27 -0400

    created dynamic slider using vue render and added component

--- assets/index.js
+++ assets/index.js
@@ -1,15 +1,60 @@
+const slider = Vue.component("slider", {
+  name: "slider",
+  props: ["data"],
+  mounted() {
+    this.$nextTick(function () {
+      new Splide(".splide").mount();
+    });
+  },
+  render(createElement) {
+    if (this.data && this.data.length) {
+      return createElement("div", { class: "splide__track" }, [
+        createElement(
+          "div",
+          { class: "splide__list" },
+          this.data.map((data) => {
+            return createElement("div", { class: "splide__slide" }, [
+              createElement(
+                "a",
+                {
+                  attrs: {
+                    href: data.url,
+                  },
+                },
+                [
+                  createElement("img", {
+                    attrs: {
+                      src: data.image,
+                    },
+                  }),
+                  createElement("h3", {}, data.title),
+                  createElement(
+                    "p",
+                    {},
+                    `Number of products: ${data.product_count}`
+                  ),
+                ]
+              ),
+            ]);
+          })
+        ),
+      ]);
+    } else {
+      return createElement("p", "No products found.");
+    }
+  },
+});
+
 const app = new Vue({
-  el: '#app',
-  delimiters: ['${', '}'],
+  el: "#app",
+  delimiters: ["${", "}"],
+  components: {
+    slider: slider,
+  },
   data: {
     collectionData: [],
-  },
-  watch: {
-    collectionData() {
-      new Splide( '.splide' ).mount();
-    }
   },
   created() {
     this.collectionData = collectionDataJson;
   },
-})
+});
